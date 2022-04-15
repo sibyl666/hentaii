b390e941941a08bce5835c360861f581cc1fe234
Sibyl <metinkorkmaz417@gmail.com>
2022-04-15 16:51:07 +0300

    Add small vertical line on left of logout / login button

--- packages/client/src/components/TheNavbar.vue
+++ packages/client/src/components/TheNavbar.vue
@@ -30,14 +30,17 @@ const toHome = () => router.push("/");
         <div class="flex gap-2" v-else>
           <button @click="toHome">Home</button>
           <button @click="toFavorites">Favorites</button>
+          <div class="bg-zinc-800 rounded w-1 h-full mx-1" />
           <TheLogout />
         </div>
       </div>
     </div>
 
+
     <div class="flex flex-col overflow-hidden md:hidden max-h-0 transition-all gap-2" :class="{ 'max-h-96 p-2': isOpen }">
       <button v-if="store.state.user.permissions?.includes('UPLOAD')" 
         @click="toUpload">Upload</button>
+
 
       <div class="flex flex-col gap-2" v-if="store.state.user.username">
         <button @click="toFavorites">Favorites</button>
